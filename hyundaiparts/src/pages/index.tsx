7cbd933bb16cfecdb352960ddf46b500f9c4b1ab
RostyslavMykhats <mirosrik@gmail.com>
2023-05-28 17:09:26 +0300

    hero

--- hyundaiparts/src/pages/index.tsx
+++ hyundaiparts/src/pages/index.tsx
@@ -1,10 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import s from "@/styles/Home.module.scss";
+import { Container, Row, Col } from "react-bootstrap";
+import ButtonUi from "@/components/button";
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -15,8 +14,42 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className={s.main}>
+        <div className={`d-flex flex-column align-items-start justify-content-start pb-5 ${s.hero}`}>
+          <Container>
+            <Row>
+              <Col xs={7}>
+                <div className={`d-flex flex-column  ${s.hero__content}`}>
+                  <h1>GIVE YOUR OLD PARTS</h1>
+                  <p className={s.hero__big}>A NEW LIFE</p>
+                  <p className="mt-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum convallis mi ut velit porttitor placerat. Nulla
+                    egestas mattis magna, ut luctus ligula mollis sit amet. Nam
+                    vitae leo dignissim, sollicitudin eros in, consequat neque.
+                    Suspendisse potenti. Quisque in est mauris. Nullam ut
+                    sollicitudin ligula.
+                  </p>
+                  <div className="d-flex gap-5 mt-5">
+                    <ButtonUi >
+                    START SHOPPING
+                    </ButtonUi>
+                    <button style={{
+                      background: "transparent",
+                      border: "1px solid #FF8F28",
+                      color: "#FF8F28",
+                      padding: "5px 20px",
+                      borderRadius: "25px"
+                    }}>
+                      SELL ITEM
+                    </button>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </main>
     </>
-  )
+  );
 }
