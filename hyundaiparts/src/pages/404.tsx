ce181507910685fc731da2e69ea3e02bb639970d
RostyslavMykhats <mirosrik@gmail.com>
2023-05-29 17:44:19 +0300

    errors

---
+++ hyundaiparts/src/pages/404.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const error = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col
+            style={{
+              minHeight: "75vh",
+              
+            }}
+            xs={12}
+            className="d-flex align-items-center justify-content-center"
+          >
+            <h1
+              style={{
+                color: "grey",
+                textAlign:'center',
+                fontSize:'80px',
+                border:'2px solid silver ',
+                borderRadius:'25px',
+                padding:'100px 40px'
+              }}
+            >
+              404 <br /> <span style={{
+                fontSize:'40px'
+              }}>PAGE NOT FOUND</span>
+            </h1>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default error;
